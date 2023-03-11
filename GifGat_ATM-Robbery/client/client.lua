58d2f643509a93179196a4879435d1c2b9ec3b58
Accidental Genius 2.0 <110291471+Aries007691@users.noreply.github.com>
2023-03-11 21:07:20 +0200

    Update client.lua
    Updated the DeleteEntity for the cashprop

--- GifGat_ATM-Robbery/client/client.lua
+++ GifGat_ATM-Robbery/client/client.lua
@@ -197,10 +197,10 @@ RegisterNetEvent('gifgat:client:cash', function(coords)
                         end
                         TaskPlayAnim(PlayerPedId(), anim, play, 8.0, 8.0, -1, 32, 0.2, 0, 0, 0)
                         Wait(1000)
-                        TriggerServerEvent('gifgat:server:Reward', cashProp, cash)
                         if DoesEntityExist(cashProp) then
                             DeleteEntity(cashProp)
                         end
+                        TriggerServerEvent('gifgat:server:Reward', cashProp, cash)
                     end,
                 }
             },
